b49aa9c3a541de72aea6fe7247ca80078f0814a1
Maciej Ciemborowicz <maciej.ciemborowicz@gmail.com>
2026-09-22 20:00:13 +0200

    test: avoid inline coverage seam attribution

--- src/coverage.h
+++ src/coverage.h
@@ -4,10 +4,7 @@
 #ifdef GIT_HOOKS_EXT_COVERAGE_TEST
 #include <stdlib.h>
 
-static int coverage_fail(const char *name)
-{
-	return getenv(name) != NULL;
-}
+#define coverage_fail(name) (getenv(name) != NULL)
 #else
 #define coverage_fail(name) 0
 #endif
